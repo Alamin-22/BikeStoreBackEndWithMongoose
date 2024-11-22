641ea850686a95e213ba917e4ff02611dc3c2007
Alamin-22 <mdalaminmollick@gmail.com>
2024-11-23 02:10:52 +0600

    updated the create product functions

--- src/App/modules/Product/Product.model.ts
+++ src/App/modules/Product/Product.model.ts
@@ -1,25 +1,50 @@
 import { Schema, model } from 'mongoose';
 import { TProduct } from './Product.interface';
 
+//  I have used Mongoose inbuilt Type Validation
+
 // creating a schema for Product
-const productSchema = new Schema<TProduct>({
-  name: { type: String, required: true },
-  brand: { type: String, required: true },
-  price: { type: Number, required: true },
-  // this is called Enum type in Mongoose => this is only used for predefined property that will never gonna change
-  category: {
-    type: String,
-    enum: {
-      values: ['Mountain', 'Road', 'Hybrid', 'Electric'],
-      message:
-        "Bike Category Must be the following =>  'Mountain', 'Road', 'Hybrid', 'Electric'",
+const productSchema = new Schema<TProduct>(
+  {
+    name: {
+      type: String,
+      required: [true, 'name is required'],
+      trim: true,
+      // custom validation and capitalize  the Name
+      validate: {
+        validator: function (value: string) {
+          const firstNameStr = value.charAt(0).toUpperCase() + value.slice(1);
+
+          return firstNameStr === value;
+        },
+        message: '{VALUE} is not in capitalized format',
+      },
     },
-    required: true,
+    brand: { type: String, required: [true, 'brand is Required'], trim: true },
+    price: { type: Number, required: [true, 'price Is Required'] },
+    // this is called Enum type in Mongoose => this is only used for predefined property that will never gonna change
+    category: {
+      type: String,
+      enum: {
+        values: ['Mountain', 'Road', 'Hybrid', 'Electric'],
+        message:
+          "Bike Category Must be the following =>  'Mountain', 'Road', 'Hybrid', 'Electric'",
+      },
+      required: true,
+    },
+    description: {
+      type: String,
+      required: [true, 'description is required'],
+      maxlength: 150,
+      trim: true,
+    },
+    quantity: { type: Number, required: [true, 'quantity is required'] },
+    inStock: { type: Boolean, required: [true, 'inStock Is Required'] },
   },
-  description: { type: String, required: true },
-  quantity: { type: Number, required: true },
-  inStock: { type: Boolean, required: true },
-});
+  {
+    timestamps: true, /// enabling Timestamps to allow createdAt and UpdatedAt filed into the Client Response
+  },
+);
 
 // 3. Create a product Model.
 
